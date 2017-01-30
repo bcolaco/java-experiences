727756630c1e50b91cabb49bbc00f8b5eaf7fd91
Bruno Colaço <bcolaco@gmail.com>
2017-01-30 20:58:07 +0000

    Added submit through keyboard.

--- HelloWorldApp/app/src/main/java/com/bcolaco/helloworldapp/MainActivity.java
+++ HelloWorldApp/app/src/main/java/com/bcolaco/helloworldapp/MainActivity.java
@@ -5,41 +5,42 @@ import android.os.*;
 import android.widget.*;
 import android.view.*;
 import android.content.*;
+import android.view.inputmethod.*;
 
 public class MainActivity extends Activity 
 {
-	//public final static String EXTRA_MESSAGE = "com.bcolaco.helloworldapp.MESSAGE";
+	private EditText editText;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		editText = (EditText) findViewById(R.id.edit_message);
+		
+		editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				boolean handled = false;
+				if (actionId == EditorInfo.IME_ACTION_SEND) {
+					submitMessage(v);
+					handled = true;
+				}
+				return handled;
+			}
+		});
     }
 	
 	public void submitMessage(View view)
 	{
-		EditText editText = (EditText) findViewById(R.id.edit_message);
-		
-		/*Intent intent = new Intent(this, DisplayActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);*/
-		
 		new AlertDialog.Builder(this)
 			.setTitle(R.string.message)
 			.setMessage(editText.getText().toString())
 			.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) { 
-					// continue with delete
 				}
 			})
-			/*.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) { 
-					// do nothing
-				}
-			})*/
 			.setIcon(android.R.drawable.ic_dialog_alert)
 			.show();
 	}
