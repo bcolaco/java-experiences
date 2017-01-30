bc17550a4fe54b64e488676ff417096839023dd9
Bruno Colaço <bcolaco@gmail.com>
2017-01-30 21:17:06 +0000

    Added log messages.

--- HelloWorldApp/app/src/main/java/com/bcolaco/helloworldapp/MainActivity.java
+++ HelloWorldApp/app/src/main/java/com/bcolaco/helloworldapp/MainActivity.java
@@ -5,15 +5,18 @@ import android.os.*;
 import android.widget.*;
 import android.view.*;
 import android.content.*;
+import android.util.*;
 import android.view.inputmethod.*;
 
 public class MainActivity extends Activity 
 {
+	private static final String TAG = "HelloWorldApp";
 	private EditText editText;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+		Log.v(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 		
@@ -22,7 +25,9 @@ public class MainActivity extends Activity
 		editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
 			@Override
 			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				Log.v(TAG, "onEditorAction");
 				boolean handled = false;
+				Log.v(TAG, "actionId=" + actionId);
 				if (actionId == EditorInfo.IME_ACTION_SEND) {
 					submitMessage(v);
 					handled = true;
@@ -34,11 +39,13 @@ public class MainActivity extends Activity
 	
 	public void submitMessage(View view)
 	{
+		Log.v(TAG, "submitMessage");
 		new AlertDialog.Builder(this)
 			.setTitle(R.string.message)
 			.setMessage(editText.getText().toString())
 			.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) { 
+				public void onClick(DialogInterface dialog, int which) {
+					Log.v(TAG, "onClick");
 				}
 			})
 			.setIcon(android.R.drawable.ic_dialog_alert)
